8305f8112f1786ce2702c18834e98b13ce727c04
Andre Eggenschwiler <swissi1984@gmail.com>
2025-02-07 13:46:12 +0100

    Freitag

--- app/src/main/java/de/syntax_institut/feels/ui/Views/ViewComponents/AnimatedCircle.kt
+++ app/src/main/java/de/syntax_institut/feels/ui/Views/ViewComponents/AnimatedCircle.kt
@@ -1,75 +1,75 @@
 package de.syntax_institut.feels.ui.Views.ViewComponents
-
-
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Surface
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
-@Composable
-fun RotatingCircle() {
-
-    var xRotation by remember {
-        mutableStateOf(0f)
-    }
-    var yRotation by remember {
-        mutableStateOf(0f)
-    }
-
-    LaunchedEffect(key1 = Unit, block = {
-        while (true) {
-            animate(
-                0f,
-                180f,
-                animationSpec = tween(800, easing = LinearOutSlowInEasing),
-                block = { value, _ -> xRotation = value }
-            )
-            animate(
-                0f,
-                180f,
-                animationSpec = tween(600, easing = LinearEasing),
-                block = { value, _ -> yRotation = value }
-            )
-        }
-    })
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .graphicsLayer {
-                    rotationX = xRotation
-                    rotationY = yRotation
-                }
-                .background(Color.White, CircleShape)
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewRotatingCircle() {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize(),
-        color = Color.Red
-    ) {
-        RotatingCircle()
-    }
-}
+//
+//
+//import androidx.compose.animation.core.LinearEasing
+//import androidx.compose.animation.core.LinearOutSlowInEasing
+//import androidx.compose.animation.core.animate
+//import androidx.compose.animation.core.tween
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.shape.CircleShape
+//import androidx.compose.material.Surface
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.graphicsLayer
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//
+//@Composable
+//fun RotatingCircle() {
+//
+//    var xRotation by remember {
+//        mutableStateOf(0f)
+//    }
+//    var yRotation by remember {
+//        mutableStateOf(0f)
+//    }
+//
+//    LaunchedEffect(key1 = Unit, block = {
+//        while (true) {
+//            animate(
+//                0f,
+//                180f,
+//                animationSpec = tween(800, easing = LinearOutSlowInEasing),
+//                block = { value, _ -> xRotation = value }
+//            )
+//            animate(
+//                0f,
+//                180f,
+//                animationSpec = tween(600, easing = LinearEasing),
+//                block = { value, _ -> yRotation = value }
+//            )
+//        }
+//    })
+//
+//    Box(
+//        modifier = Modifier.fillMaxSize(),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Box(
+//            modifier = Modifier
+//                .size(100.dp)
+//                .graphicsLayer {
+//                    rotationX = xRotation
+//                    rotationY = yRotation
+//                }
+//                .background(Color.White, CircleShape)
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewRotatingCircle() {
+//    Surface(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        color = Color.Red
+//    ) {
+//        RotatingCircle()
+//    }
+//}
